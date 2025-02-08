c8785254e90b56943c5e094528e96acb9e094f09
Sharon812 <sharusharon1110@gmail.com>
2025-02-08 23:38:40 +0530

    implemented add to wishlist on productdetails page

--- controller/users/productController.js
+++ controller/users/productController.js
@@ -1,6 +1,7 @@
 const Products = require("../../models/productSchema");
 const category = require("../../models/categorySchema");
 const User = require("../../models/userSchema");
+const Wishlist = require("../../models/wishlistSchema");
 
 const productDetails = async (req, res) => {
   try {
@@ -26,13 +27,22 @@ const productDetails = async (req, res) => {
     const productOffer = product.productOffer || 0;
     const totalOffer = categoryOffer + productOffer;
 
+    let wishlistProducts = [];
+
     if (user) {
+      const wishlist = await Wishlist.findOne({ userId: user }).select(
+        "product.productId"
+      );
+      wishlistProducts = wishlist
+        ? wishlist.product.map((item) => item.productId.toString())
+        : [];
       res.render("productDetail", {
         product: product,
         category: findCategory,
         totalOffer: totalOffer,
         relatedProducts: relatedProducts,
         user: userData,
+        wishlistProducts,
       });
     } else {
       res.render("productDetail", {
@@ -40,6 +50,7 @@ const productDetails = async (req, res) => {
         category: findCategory,
         totalOffer: totalOffer,
         relatedProducts: relatedProducts,
+        wishlistProducts,
       });
     }
   } catch (error) {
